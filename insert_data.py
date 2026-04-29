695af038068cba7146f6b8d0ed3c4ff805a4b07e
Dima Kozlovskyy <dima.kozlovskyy@gmail.com>
2026-04-29 16:19:37 +0000

    updated insert_data.py , added first book to wsaa_db

--- insert_data.py
+++ insert_data.py
@@ -1,15 +1,17 @@
 import mysql.connector
-db = mysql.connector.connect(
- host="localhost",
- user="root",
- password="",
- database="wsaa"
-)
+from config import config
+
+# Using ** to unpack the config dictionary and pass the values as keyword arguments to the connect() function:
+db = mysql.connector.connect(**config)
+
+
 cursor = db.cursor()
-sql="insert into student (name, age) values (%s,%s)"
-values = ("Mary",21)
+
+sql="INSERT INTO books (title, author, isbn, price) VALUES (%s, %s, %s, %s)"
+values = ("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 10.99)
+
 cursor.execute(sql, values)
 db.commit()
 print("1 record inserted, ID:", cursor.lastrowid)
-mycursor.close()
-connection.close()
+cursor.close()
+db.close()
