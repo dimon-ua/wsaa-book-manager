deed4d52bc91d46e61d399f7b2485e1726e7258e
Dima Kozlovskyy <dima.kozlovskyy@gmail.com>
2026-05-13 11:54:59 +0000

    removed redirect, changed on API REST (get, post, put, delete)

--- app.py
+++ app.py
@@ -1,26 +1,81 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from booksDAO import booksDAO
-import mysql.connector
-from config import config
-from flask import jsonify, request
 
-
+# Reference: Create quick Flask app
+# https://flask.palletsprojects.com/en/2.3.x/quickstart/
 app = Flask(__name__)
 
 
 #------- ROUTES --------
-@app.route("/")
-@app.route("/index")
+# Reference: Flask routing 
+# https://flask.palletsprojects.com/en/stable/quickstart/#routing
+
+# 'index' root, main page
+@app.route("/") 
 def index():
     books = booksDAO.get_all_books()
     all_authors = booksDAO.get_all_authors()
     return render_template("index.html", books=books, all_authors=all_authors)
 
+# ****************************GET ALL***************************
+# REST API route to get all books in JSON format
+# Reference:  https://flask.palletsprojects.com/en/2.3.x/api/#flask.json.jsonify
+# https://www.geeksforgeeks.org/python/flask-creating-rest-apis/
+@app.route("/api/books", methods=["GET"])
+def get_all_books_api():
+    books = booksDAO.get_all_books()
+    return jsonify(books)
 
-@app.route("/auth")
-def auth():
-    return render_template("auth.html")
+
+# ****************************POST***************************
+# Reference:
+# https://flask.palletsprojects.com/en/stable/api/#flask.Request.get_json
+@app.route("/api/books", methods=["POST"])
+def add_book_api():
+    data = request.get_json()
+    title = data.get("title")
+    author = data.get("author")
+    price = data.get("price")
+    isbn = data.get("isbn")
+
+    new_book_id = booksDAO.add_book(title, author, price, isbn)
+   
+    return jsonify({"id": new_book_id, "message": "Book added successfully"}), 201
+
+
+# ****************************PUT***************************
+@app.route("/api/books/<int:book_id>", methods=["PUT"])
+def update_book_api(book_id):
+    data = request.get_json()
+    title = data.get("title")
+    author = data.get("author")
+    price = data.get("price")
+    isbn = data.get("isbn")
+
+    booksDAO.update_book(book_id, title, author, price, isbn)
     
+    return jsonify({"message": "Book updated successfully"})
+
+
+# Route to uodate the book
+@app.route("/update_book/<int:book_id>", methods=["POST"])
+def update_book(book_id):
+    title = request.form.get("title")
+    author = request.form.get("author")
+    price = request.form.get("price")
+    isbn = request.form.get("isbn")
+    booksDAO.update_book(book_id, title, author, price, isbn)
+    return redirect("/")
+
+
+
+# ****************************DELETE***************************
+@app.route("/api/books/<int:book_id>", methods=["DELETE"])
+def delete_book_api(book_id):
+    booksDAO.delete_book(book_id)
+    return jsonify({"message": "Book deleted successfully"})
+
+
 
 @app.route("/api/books/<int:id>", methods=["GET"])
 def get_book_api(id):
@@ -30,21 +85,6 @@ def get_book_api(id):
     else:
         return jsonify({"error": "Book not found"}), 404
 
-@app.route("/add_book", methods=["POST"])
-def add_book():
-    title = request.form.get("title")
-    author = request.form.get("author")
-    price = request.form.get("price")
-    isbn = request.form.get("isbn")
-
-    new_book_id = booksDAO.add_book(title, author, price, isbn)   
-   
-    return redirect("/")
-
-@app.route("/delete_book/<int:id>")
-def delete_book(id):
-    booksDAO.delete_book(id)
-    return redirect("/")
 
 
 @app.route("/edit_book/<int:book_id>")
@@ -53,6 +93,8 @@ def edit_book(book_id):
     book = booksDAO.get_book_by_id(book_id)
     return render_template("edit_book.html", book=book)
     
+
+
 
 #------- FLASK RUNNING CODE --------
 if __name__ == "__main__":
